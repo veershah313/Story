e3114c9977731e3b526cb4e6344adf7116fe7ede
Veer Shah <veer.shah.uk@gmail.com>
2020-05-08 14:50:08 +0100

    Add project files.

---
+++ Story/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.VisualBasic;
+using System;
+using System.Threading.Tasks.Dataflow;
+
+namespace Story
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            string age = Console.ReadLine();
+            Console.WriteLine("What is your superpower?");
+            string power = Console.ReadLine();
+            Console.WriteLine("What is your unbeatable weapon?");
+            string weapon = Console.ReadLine();
+            Console.WriteLine("Who is your nemesis?");
+            string enemy = Console.ReadLine();
+            Console.WriteLine("");
+
+
+
+           
+        }
+    }
+}
